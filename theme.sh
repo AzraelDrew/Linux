4ed9e9cd94099b86205e91c7e0aabe2ae311b08c
Azrael <740161192@qq.com>
2021-07-15 20:57:30 +0800

    init

---
+++ theme.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/vinceliuice/WhiteSur-gtk-theme.git;
+cd WhiteSur-gtk-theme;
+sudo ./install.sh ;
+sudo ./tweak.sh -f;
+cd ..;
+git clone https://github.com/vinceliuice/WhiteSur-icon-theme.git;
+cd WhiteSur-icon-theme;
+sudo ./install.sh ;
