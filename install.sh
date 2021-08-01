53a430066e444ac06c810050b639ab946dc9deeb
Azrael <740161192@qq.com>
2021-08-01 21:39:05 +0800

    update

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
-sudo apt install vim wget curl telegram-desktop gnome-sushi gnome-tweak-tool flameshot vlc grub-customizer scrcpy samba ;
+sudo add-apt-repository ppa:apandada1/brightness-controller ;
+sudo apt install vim wget curl telegram-desktop gnome-sushi gnome-tweak-tool flameshot vlc grub-customizer scrcpy samba sl cmatrix brightness-controller-simple ;
 sudo echo "set completion-ignore-case on" >>  /etc/inputrc;
